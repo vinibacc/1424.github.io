730ac50008263049dfbf6f4e6b761f3bf8c66abd
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2020-01-27 16:24:34 -0300

    ajust

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-window.onscroll = function() {
-    myFunction()
-};
-var header = document.getElementById("menu");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky &&
-        break) {
-        header.classList.add("sticky");
-        $(".navbar-light .navbar-nav .nav-link").css("color", "#000");
-    } else {
-        header.classList.remove("sticky");
-        $(".navbar-light .navbar-nav .nav-link").css("color", "#fff");
-    }
-}
-
 $('#carousel-1').carousel({
     interval: 4000,
     wrap: true,
---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    myFunction()
+};
+var header = document.getElementById("menu");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky &&
+        breakpoint) {
+        header.classList.add("sticky");
+        $(".navbar-light .navbar-nav .nav-link").css("color", "#000");
+    } else {
+        header.classList.remove("sticky");
+        $(".navbar-light .navbar-nav .nav-link").css("color", "#fff");
+    }
+}
