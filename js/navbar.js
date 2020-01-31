6cbec0095a6552e5d415712194e546eaf6fcdcfb
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2020-01-31 09:04:11 -0300

    ajust

--- js/navbar.js
+++ js/navbar.js
@@ -6,8 +6,7 @@ var header = document.getElementById("menu");
 var sticky = header.offsetTop;
 
 function myFunction() {
-    if (window.pageYOffset > sticky &&
-        breakpoint) {
+    if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
         $(".navbar-light .navbar-nav .nav-link").css("color", "#000");
     } else {
